5eb0062e04c580e4cbed8883bda38228dc779c70
Akos Szalay <akos.szalay@gmail.com>
2020-10-18 17:40:32 +0100

    added i18n

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
@@ -23,8 +23,8 @@ module.exports = {
         objects: "only-multiline",
         imports: "never",
         exports: "only-multiline",
-        functions: "never",
-      },
-    ],
-  },
+        functions: "never"
+      }
+    ]
+  }
 };
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,23 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
+import VueI18n from "vue-i18n";
+import en from "vuetify/src/locale/en";
+import hu from "vuetify/src/locale/hu";
+
 import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify);
+Vue.use(VueI18n);
+
+const messages = {
+  en: { $vuetify: en },
+  hu: { $vuetify: hu }
+};
+
+const i18n = new VueI18n({
+  locale: "en", // set locale
+  messages // set locale messages
+});
 
 export default new Vuetify({
   theme: {
@@ -17,5 +32,10 @@ export default new Vuetify({
         warning: "#FFC107"
       }
     }
+  },
+  lang: {
+    t: (key, ...params) => i18n.t(key, params) as string,
+    locales: { en, hu },
+    current: "en"
   }
 });
